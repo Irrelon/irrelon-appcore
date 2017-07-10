d34a0489017823522c3b7bbb2338f3f297cd480e
irrelon <rob@irrelon.com>
2017-07-11 00:52:28 +0100

    Added data object that can be used for any application storage

--- js/AppCore.js
+++ js/AppCore.js
@@ -31,6 +31,7 @@ var AppCore = function () {
 	this._moduleDefs = {};
 	this._config = [];
 	this._run = [];
+	this.data = {};
 	
 	// The object that holds a reference to callbacks that
 	// are waiting for a module to become available / loaded
